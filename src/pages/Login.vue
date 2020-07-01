c6da73cc9087fdcb53a1dfd700f1d66eb60efd2d
zhangtingwei <zhangtingwei2@gome.com.cn>
2020-07-01 18:15:46 +0800

    first commit

---
+++ src/pages/Login.vue
@@ -0,0 +1,114 @@
+<template>
+  <div class="login">
+    <div class="back">
+      <router-link to="/start">
+        <svg xmlns="http://www.w3.org/2000/svg" class="svg-icon" viewBox="0 0 1024 1024" width="24" height="24" style="fill: rgb(16, 16, 16);"><path d="M872 572H266.8l144.3-183c4.1-5.2.4-13-6.3-13H340c-9.8 0-19.1 4.5-25.1 12.2l-164 208c-16.5 21-1.6 51.8 25.1 51.8h696c4.4 0 8-3.6 8-8v-60c0-4.4-3.6-8-8-8z"></path></svg>
+      </router-link>
+    </div>
+    <div class="welcome">
+      欢迎来到摄图
+    </div>
+    <div class="login-form">
+      <div class="ipt_outer">
+        <div class="tel-icon">
+          <svg xmlns="http://www.w3.org/2000/svg" class="svg-icon" viewBox="0 0 32 32" width="22" height="22" style="fill: rgb(16, 16, 16);"><path d="M8 5.333v21.333h16v-21.333h-16zM6.667 2.667h18.667c0.736 0 1.333 0.597 1.333 1.333v0 24c0 0.736-0.597 1.333-1.333 1.333v0h-18.667c-0.736 0-1.333-0.597-1.333-1.333v0-24c0-0.736 0.597-1.333 1.333-1.333v0zM16 22.667c0.736 0 1.333 0.597 1.333 1.333s-0.597 1.333-1.333 1.333v0c-0.736 0-1.333-0.597-1.333-1.333s0.597-1.333 1.333-1.333v0z"></path></svg>
+          <svg xmlns="http://www.w3.org/2000/svg" style="overflow: visible; filter: none;" width="1" height="22"><path d="M 0.5 0 L 0.5 14" stroke="rgba(221,221,221,1)" stroke-width="1px" stroke-dasharray=""></path><path d="M 0.5 0 L 0.5 14" stroke="transparent" stroke-width="10" style="pointer-events: auto;"></path></svg>
+        </div>
+        <div  class="ipt">
+          <x-input type="tel" placeholder="请输入手机号码" required></x-input>
+        </div>
+      </div>
+      <div class="ipt_outer">
+        <div class="tel-icon">
+         <svg xmlns="http://www.w3.org/2000/svg" class="svg-icon" viewBox="0 0 32 32" width="22" height="22" style="fill: rgb(16, 16, 16);"><path d="M16 1.333l10.956 2.435c0.602 0.137 1.044 0.668 1.044 1.301v0 13.316c-0 2.764-1.402 5.201-3.534 6.638l-0.028 0.018-8.437 5.625-8.437-5.625c-2.16-1.455-3.562-3.891-3.563-6.655v-13.317c0-0.634 0.442-1.164 1.035-1.3l0.009-0.002 10.956-2.435zM16 4.065l-9.333 2.073v12.247c0 1.843 0.935 3.467 2.356 4.425l0.019 0.012 6.959 4.64 6.959-4.64c1.44-0.97 2.374-2.594 2.375-4.436v-12.248l-9.333-2.072zM16 9.333c0 0 0.001 0 0.001 0 1.473 0 2.667 1.194 2.667 2.667 0 0.982-0.531 1.84-1.321 2.303l-0.013 0.007-0.001 5.691h-2.667v-5.691c-0.803-0.47-1.333-1.328-1.333-2.309 0-1.473 1.194-2.667 2.667-2.667v0z"></path></svg>
+          <svg xmlns="http://www.w3.org/2000/svg" style="overflow: visible; filter: none;" width="1" height="22"><path d="M 0.5 0 L 0.5 14" stroke="rgba(221,221,221,1)" stroke-width="1px" stroke-dasharray=""></path><path d="M 0.5 0 L 0.5 14" stroke="transparent" stroke-width="10" style="pointer-events: auto;"></path></svg>
+        </div>
+         <div  class="ipt">
+          <x-input type="text" placeholder="请输入验证码" required></x-input>
+        </div>
+        <div class="getCode"> 获取验证码</div>
+      </div>
+      <div class="info">
+        未注册的手机号验证后自动创建摄图账号
+      </div>
+      <div class="ipt_outer">
+        <x-button class="login_btn">登录</x-button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { Group, XInput, XButton  } from 'vux'
+  export default {
+    components: { Group, XInput, XButton  },
+
+  }
+</script>
+
+<style lang="less" scoped>
+  .login {
+    height: 100vh;
+    .back {
+      width: 24px;
+      height: 24px;
+    }
+    .welcome {
+      width: 100%;
+      font-family: PingFangSC;
+      font-weight: bold;
+      font-size: 26px;
+      color: rgb(16, 16, 16);
+      text-align: center;
+      height: 100px;
+      line-height: 100px;
+    }
+    .ipt_outer {
+      position: relative;
+      border-radius: 50px;
+      background-color: rgb(245, 245, 245);
+      margin: 20px 20px;
+      .tel-icon {
+        position: absolute;
+        left: 0;
+        top: center;
+        display: inline-block;
+        // width: 20px;
+        top: 12px;
+        left: 14px;
+        // border-right: 1px solid;
+
+      }
+      .ipt {
+        display: inline-block;
+        margin-left: 40px;
+      }
+      .getCode {
+        position: absolute;
+        display: inline-block;
+        background-color: rgb(16, 16, 16);
+        border-radius: 50px;
+        font-size: 12px;
+        padding: 5px 20px;
+        color: #fff;
+        top: 8px;
+        right: 0;
+      }
+      .login_btn {
+        background-color: rgb(16, 16, 16);
+        color: #fff;
+        border-radius: 50px;
+        font-size: 16px;
+        height: 45px;
+      }
+      
+    }
+    .info {
+      font-family: PingFangSC;
+      font-weight: 400;
+      font-size: 13px;
+      color: rgba(200, 200, 200, 1);
+      text-align: center;
+    }
+  }
+</style>
---
+++ src/pages/Start.vue
@@ -0,0 +1,104 @@
+<template>
+  <div class="start">
+    <div class="start_action">
+      <span class="phone">
+        <svg xmlns="http://www.w3.org/2000/svg" class="svg-icon" viewBox="64 64 896 896" width="60" height="60" style="fill: rgb(255, 255, 255);"><path d="M512 306.9c-113.5 0-205.1 91.6-205.1 205.1S398.5 717.1 512 717.1 717.1 625.5 717.1 512 625.5 306.9 512 306.9zm0 338.4c-73.4 0-133.3-59.9-133.3-133.3S438.6 378.7 512 378.7 645.3 438.6 645.3 512 585.4 645.3 512 645.3zm213.5-394.6c-26.5 0-47.9 21.4-47.9 47.9s21.4 47.9 47.9 47.9 47.9-21.3 47.9-47.9a47.84 47.84 0 0 0-47.9-47.9zM911.8 512c0-55.2.5-109.9-2.6-165-3.1-64-17.7-120.8-64.5-167.6-46.9-46.9-103.6-61.4-167.6-64.5-55.2-3.1-109.9-2.6-165-2.6-55.2 0-109.9-.5-165 2.6-64 3.1-120.8 17.7-167.6 64.5C132.6 226.3 118.1 283 115 347c-3.1 55.2-2.6 109.9-2.6 165s-.5 109.9 2.6 165c3.1 64 17.7 120.8 64.5 167.6 46.9 46.9 103.6 61.4 167.6 64.5 55.2 3.1 109.9 2.6 165 2.6 55.2 0 109.9.5 165-2.6 64-3.1 120.8-17.7 167.6-64.5 46.9-46.9 61.4-103.6 64.5-167.6 3.2-55.1 2.6-109.8 2.6-165zm-88 235.8c-7.3 18.2-16.1 31.8-30.2 45.8-14.1 14.1-27.6 22.9-45.8 30.2C695.2 844.7 570.3 840 512 840c-58.3 0-183.3 4.7-235.9-16.1-18.2-7.3-31.8-16.1-45.8-30.2-14.1-14.1-22.9-27.6-30.2-45.8C179.3 695.2 184 570.3 184 512c0-58.3-4.7-183.3 16.1-235.9 7.3-18.2 16.1-31.8 30.2-45.8s27.6-22.9 45.8-30.2C328.7 179.3 453.7 184 512 184s183.3-4.7 235.9 16.1c18.2 7.3 31.8 16.1 45.8 30.2 14.1 14.1 22.9 27.6 30.2 45.8C844.7 328.7 840 453.7 840 512c0 58.3 4.7 183.2-16.2 235.8z"></path></svg>
+      </span>
+      <div class="start_word">
+        <span>打开生活，发现美好</span>
+      </div>
+      <div>
+        <router-link to="/login">
+         <div class="circle">
+
+          <svg xmlns="http://www.w3.org/2000/svg" class="svg-icon" viewBox="0 0 32 32" width="24" height="24" style="fill: rgb(255, 255, 255);"><path d="M21.563 14.667l-7.152-7.152 1.885-1.885 10.371 10.371-10.371 10.371-1.885-1.885 7.152-7.152h-16.229v-2.667z"></path></svg>
+        </div>
+        </router-link>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style lang="less" scoped>
+  .start {
+    width: 100%;
+    // height: 100%;
+    height: 100vh;
+    text-align: center;
+    background-image: url('/static/images/start.jpg');
+    background-size: 100% 100%;
+    .start_action {
+      position: absolute;
+      // height: 400px;
+      left: 0;
+      right: 0;
+      top: 280px;
+      bottom: 0
+    }
+    .phone {
+      // animation: name duration timing-function delay iteration-count direction fill-mode;
+      // animation:turn 1s linear infinite;    
+      // transition: all 2s ease;
+      svg {
+        transform-origin: left bottom;
+        // transform: rotate(-40deg);
+        animation: turn 0.5s linear 1;    
+
+      }
+    }
+    .animated {
+      // -webkit-animation-duration: 1s;
+      // animation-duration: 1s;
+      // -webkit-animation-fill-mode: both;
+      // animation-fill-mode: both;
+    }
+   
+    .start_word {
+      color: #fff;
+      font-size: 15px;
+      font-weight: bold;
+      font-family: PingFangSC;
+      margin: 20px 0;
+    }
+    .circle {
+      width: 54px;
+      height: 54px;
+      border-radius: 50%;
+      background-color: rgb(232, 52, 52);
+      box-shadow: rgba(232, 52, 52, 0.61) 0px 0px 50px 0px;
+      display: inline-block;
+      text-align: center;
+      line-height: 54px;
+      animation: move 0.5s linear 1;    
+      svg {
+        height: 54px;
+      }
+    }
+     /* 
+      turn : 定义的动画名称
+      1s : 动画时间
+      linear : 动画以何种运行轨迹完成一个周期
+      infinite :规定动画应该无限次播放
+     */
+    @keyframes turn{
+      0%{-webkit-transform:rotate(-40deg);}
+      100%{-webkit-transform:rotate(0deg);}
+    }
+     /* 
+      move : 定义的动画名称
+      1s : 动画时间
+      linear : 动画以何种运行轨迹完成一个周期
+      infinite :规定动画应该无限次播放
+     */
+    @keyframes move{
+      0%{-webkit-transform:translateX(-30px);}
+      100%{-webkit-transform:translateX(0);}
+    }
+  }
+</style>
